3934bdb5555ff24bb3ed2117801410f2cc4f8341
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-22 00:18:52 +0300

    removed files with react-icons logic

--- client/src/assets/icons/iconStyles.ts
+++
@@ -1,8 +0,0 @@
-import { IconContext } from "react-icons"
-import colors from "styles/colors.module.scss"
-
-const headerIconStyles: IconContext = {
-  size: "50px",
-  color: colors.white,
-}
-export { headerIconStyles }
