098e67a0f0f57532c6d48a5faa1f3b7bf5d171e0
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-22 00:24:41 +0300

    added layout for Auth page

--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -1,9 +1,16 @@
 import React, { FC } from "react"
+import AuthModal from "components/AuthModal/AuthModal"
+
+import styles from "./AuthPage.module.scss"
 
 interface AuthPageProps {}
 
 const AuthPage: FC<AuthPageProps> = () => {
-  return <div></div>
+  return (
+    <div className={styles.container}>
+      <AuthModal />
+    </div>
+  )
 }
 
 export default AuthPage
