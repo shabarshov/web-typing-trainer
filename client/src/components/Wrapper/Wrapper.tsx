f8594710bc3f3ffef575bc466507544dcea00a40
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-22 00:25:39 +0300

    updated Wrapper component

--- client/src/components/Wrapper/Wrapper.tsx
+++ client/src/components/Wrapper/Wrapper.tsx
@@ -2,9 +2,9 @@ import React, { FC } from "react"
 
 import { Link } from "react-router-dom"
 
-import Icon from "components/Icon/Icon"
-import { headerIconStyles } from "assets/icons/iconStyles"
-import { BsHouse, BsGear, BsGraphUp } from "assets/icons/reactIcons"
+import ToolTip from "components/Tooltip/Tooltip"
+import * as Icons from "assets/svg/Wrapper"
+import cn from "classnames"
 
 import styles from "./Wrapper.module.scss"
 
@@ -15,25 +15,37 @@ interface WrapperProps {
 const Wrapper: FC<WrapperProps> = ({ children }) => {
   return (
     <div className={styles.wrapper}>
-      <header>
-        <Link to="/">
-          <Icon
-            icon={<BsHouse />}
-            iconStyles={headerIconStyles}
-          />
-        </Link>
-        <Link to="/settings">
-          <Icon
-            icon={<BsGear />}
-            iconStyles={headerIconStyles}
-          />
-        </Link>
-        <Link to="/statistic">
-          <Icon
-            icon={<BsGraphUp />}
-            iconStyles={headerIconStyles}
-          />
-        </Link>
+      <header className={styles.header}>
+        <div className={styles.left}>
+          <Icons.LogoIcon className={styles.logo} />
+          <Link to="/">
+            <ToolTip value="Home">
+              <Icons.HomeIcon className={styles.icon} />
+            </ToolTip>
+          </Link>
+          <Link to="/statistic">
+            <ToolTip value="Rating">
+              <Icons.RatingIcon className={styles.icon} />
+            </ToolTip>
+          </Link>
+          <Link to="/settings">
+            <ToolTip value="Settings">
+              <Icons.SettingsIcon
+                className={cn(styles.icon, styles.gearRotate)}
+              />
+            </ToolTip>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <Link to="/profile">
+            <ToolTip value="Profile">
+              <Icons.ProfileIcon className={styles.icon} />
+            </ToolTip>
+          </Link>
+          <ToolTip value="Sign out">
+            <Icons.SignOutIcon className={styles.icon} />
+          </ToolTip>
+        </div>
       </header>
       <div className={styles.page}>{children}</div>
     </div>
