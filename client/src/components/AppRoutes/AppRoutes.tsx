aeeaef96d186012816525eb6b2d5699526acb837
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-25 20:40:04 +0300

    added className prop

--- client/src/components/AppRoutes/AppRoutes.tsx
+++ client/src/components/AppRoutes/AppRoutes.tsx
@@ -1,17 +1,21 @@
-import type { FC } from "react"
 import React from "react"
+import type { FC } from "react"
 
 import { Routes, Route } from "react-router-dom"
-import type { IRoute } from "routes/routes"
 import { authRoutes } from "routes/routes"
 
-const AppRoutes: FC = () => {
+import type { IRoute } from "routes/routes"
+import type { AppRoutesProps } from "./AppRoutesProps"
+
+const AppRoutes: FC<AppRoutesProps> = ({ className }) => {
   return (
-    <Routes>
-      {authRoutes.map(({ path, Component }: IRoute) => (
-        <Route key={path} path={path} element={Component} />
-      ))}
-    </Routes>
+    <div className={className}>
+      <Routes>
+        {authRoutes.map(({ path, Component }: IRoute) => (
+          <Route key={path} path={path} element={Component} />
+        ))}
+      </Routes>
+    </div>
   )
 }
 
