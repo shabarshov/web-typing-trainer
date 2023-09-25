123e2734e2e1c22fdba1c45b6bf3f41aa55f5c7f
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-25 03:18:48 +0300

    updated AuthInput

--- client/src/components/AuthInput/AuthInput.tsx
+++ client/src/components/AuthInput/AuthInput.tsx
@@ -1,18 +1,25 @@
 import React from "react"
 import type { FC } from "react"
 
+import Input from "components/UI/Input/Input"
+
 import type { AuthInputProps } from "./AuthInputProps"
 
 import styles from "./AuthInput.module.scss"
 
-const AuthInput: FC<AuthInputProps> = ({ title, placeholder }) => {
+const AuthInput: FC<AuthInputProps> = ({
+  title,
+  placeholder,
+  type = "text",
+}) => {
   return (
     <div className={styles.container}>
       <span className={styles.title}>{title?.toUpperCase()}</span>
-      <input
+      <Input
         className={styles.input}
         placeholder={placeholder}
         spellCheck={false}
+        type={type}
       />
     </div>
   )
