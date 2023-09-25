5d6f76e31193d6f0110f09a26c66235c87c05a4e
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-25 20:39:34 +0300

    updated App

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -2,16 +2,18 @@ import type { FC } from "react"
 import React from "react"
 
 import AppRoutes from "components/AppRoutes/AppRoutes"
-import Wrapper from "../Wrapper/Wrapper"
+import WrapperContainer from "components/containers/WrapperContainer/WrapperContainer"
+import NavBar from "components/NavBar/NavBar"
 
 import styles from "./App.module.scss"
 
 const App: FC = () => {
   return (
     <div className={styles.container}>
-      <Wrapper>
-        <AppRoutes />
-      </Wrapper>
+      <WrapperContainer>
+        <NavBar />
+        <AppRoutes className={styles.page} />
+      </WrapperContainer>
     </div>
   )
 }
