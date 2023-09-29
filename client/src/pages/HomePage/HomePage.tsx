85f11b89e1045a4b592ff2413d39d2d1540d212e
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-29 19:24:58 +0300

    updated HomePage (added typing logic)

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,22 @@
+import React, { useState } from "react"
 import type { FC } from "react"
-import React from "react"
+
+import Caret from "components/Caret/Caret"
+import TextContainer from "components/containers/TextContainer/TextContainer"
+import TextField from "components/TextField/TextField"
 
 const HomePage: FC = () => {
-  return <div></div>
+  const [caretPosition, setCaretPosition] = useState({
+    left: "-1px",
+    top: "0px",
+  })
+
+  return (
+    <TextContainer>
+      <Caret left={caretPosition.left} top={caretPosition.top} />
+      <TextField setCaretPosition={setCaretPosition} />
+    </TextContainer>
+  )
 }
 
 export default HomePage
