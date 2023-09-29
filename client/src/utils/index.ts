c3ca8753b585c41c2d4955f081dc3f48ae0040cc
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-29 19:23:28 +0300

    added re-export for utils

---
+++ client/src/utils/index.ts
@@ -0,0 +1,13 @@
+import createCounter from "utils/createCounter"
+import parseText from "utils/parseText"
+import readString from "utils/readString"
+import updateCaretPosition from "utils/updateCaretPosition"
+import getTextRowLengths from "utils/getTextRowLengths"
+
+export {
+  createCounter,
+  parseText,
+  readString,
+  updateCaretPosition,
+  getTextRowLengths,
+}
