5acad64dce65b6f3dba49fee3a300acfd9183b9e
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-09-29 19:21:16 +0300

    added parseText util

---
+++ client/src/utils/parseText.ts
@@ -0,0 +1,13 @@
+const parseText = (str: string): string[] => {
+  const text: string[] = str.split(" ")
+  const lastIndex: number = text.length
+
+  const result: string[] = text.map((word, index) => {
+    if (index !== lastIndex) return word + " "
+    return word
+  })
+
+  return result
+}
+
+export default parseText
