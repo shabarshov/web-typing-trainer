c9511641e03d45cdfb062b9e9c6faca0d1795bdd
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-04 02:41:19 +0300

    updated icon colors, Links replaced to NavLinks

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -4,41 +4,43 @@ import type { FC } from "react"
 import * as Icons from "assets/svg/Wrapper"
 import ToolTip from "components/Tooltip/Tooltip"
 
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 import cn from "classnames"
 
 import styles from "./NavBar.module.scss"
 
 const NavBar: FC = () => {
+  const className = ({ isActive }: { isActive: boolean }): string => {
+    return cn(isActive ? styles.active : "", styles.icon)
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.left}>
         <Icons.LogoIcon className={styles.logo} />
-        <Link to="/">
+        <NavLink to="/" className={className}>
           <ToolTip value="Home">
-            <Icons.HomeIcon className={styles.icon} />
+            <Icons.HomeIcon />
           </ToolTip>
-        </Link>
-        <Link to="/statistic">
+        </NavLink>
+        <NavLink to="/statistic" className={className}>
           <ToolTip value="Rating">
-            <Icons.RatingIcon className={styles.icon} />
+            <Icons.RatingIcon />
           </ToolTip>
-        </Link>
-        <Link to="/settings">
+        </NavLink>
+        <NavLink to="/settings" className={className}>
           <ToolTip value="Settings">
-            <Icons.SettingsIcon
-              className={cn(styles.icon, styles.gearRotate)}
-            />
+            <Icons.SettingsIcon className={styles.gearRotate} />
           </ToolTip>
-        </Link>
+        </NavLink>
       </div>
       <div className={styles.right}>
-        <Link to="/profile">
+        <NavLink to="/profile" className={className}>
           <ToolTip value="Profile">
-            <Icons.ProfileIcon className={styles.icon} />
+            <Icons.ProfileIcon />
           </ToolTip>
-        </Link>
+        </NavLink>
         <ToolTip value="Sign out">
           <Icons.SignOutIcon className={styles.icon} />
         </ToolTip>
