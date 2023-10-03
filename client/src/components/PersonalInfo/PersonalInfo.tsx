68897117bb9016265c4bd48ee3f6f77eb52b38dc
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-04 02:18:34 +0300

    updated PersonalInfo

--- client/src/components/PersonalInfo/PersonalInfo.tsx
+++ client/src/components/PersonalInfo/PersonalInfo.tsx
@@ -1,20 +1,23 @@
 import React from "react"
 
 import Tooltip from "components/Tooltip/Tooltip"
-import * as Icons from "assets/svg/Profile"
 import Text from "components/UI/Text/Text"
+import * as Icons from "assets/svg/Profile"
 
 import styles from "./PersonalInfo.module.scss"
 
 const PersonalInfo = () => {
   return (
     <div className={styles.container}>
-      <Icons.ProfileIcon className={styles.icon} />
+      <div className={styles.iconContainer}>
+        <Icons.ProfileIcon className={styles.icon} />
+        <Text className={styles.date} value="Joined: 25.02.2004" />
+      </div>
+
       <div className={styles.personInfo}>
         <Tooltip value="edit">
           <Icons.EditIcon className={styles.editIcon} />
         </Tooltip>
-
         <Text className={styles.login} value="VadimSex228" />
         <Text className={styles.email} value="alexey_shabarshov@mail.ru" />
       </div>
