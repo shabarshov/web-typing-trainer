4a1d9f0165364d03e4f35752f95a0869111c5999
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-04 02:19:16 +0300

    updated ProfilePage

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -4,13 +4,31 @@ import React from "react"
 import ProfileContainer from "components/containers/ProfileContainer/ProfileContainer"
 
 import PersonalInfo from "components/PersonalInfo/PersonalInfo"
+import Achievement from "components/Achievement/Achievement"
+import ScrollableContainer from "components/containers/ScrollableContainer/ScrollableContainer"
 import ProfileStatistics from "components/ProfileStatistics/ProfileStatistics"
+
+import cn from "classnames"
+
+import styles from "./ProfilePage.module.scss"
 
 const ProfilePage: FC = () => {
   return (
     <ProfileContainer>
-      <PersonalInfo />
-      <ProfileStatistics />
+      <div className={styles.left}>
+        <PersonalInfo />
+        <ProfileStatistics />
+      </div>
+
+      <ScrollableContainer className={cn(styles.achievements, styles.right)}>
+        <Achievement isPassed={true} />
+        <Achievement isPassed={true} />
+        <Achievement isPassed={true} />
+        <Achievement isPassed={false} />
+        <Achievement isPassed={false} />
+        <Achievement isPassed={false} />
+        <Achievement isPassed={false} />
+      </ScrollableContainer>
     </ProfileContainer>
   )
 }
