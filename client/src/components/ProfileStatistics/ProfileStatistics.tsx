1bc35c072caaeb2e381dc8e7254fbf3650c08fe1
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-04 02:18:55 +0300

    updated ProfileStatistics

--- client/src/components/ProfileStatistics/ProfileStatistics.tsx
+++ client/src/components/ProfileStatistics/ProfileStatistics.tsx
@@ -1,28 +1,15 @@
 import type { FC } from "react"
 import React from "react"
 
-import TitledCard from "components/UI/TitledCard/TitledCard"
-import Text from "components/UI/Text/Text"
+import TitledCard from "components/TitledCard/TitledCard"
 
 import styles from "./ProfileStatistics.module.scss"
 
 const ProfileStatistics: FC = () => {
   return (
     <div className={styles.container}>
-      <TitledCard>
-        <Text value="Joined" />
-        <Text value="18.09.2023" />
-      </TitledCard>
-
-      <TitledCard>
-        <Text value="Best speed" />
-        <Text value="643" />
-      </TitledCard>
-
-      <TitledCard>
-        <Text value="Average speed" />
-        <Text value="531" />
-      </TitledCard>
+      <TitledCard className={styles.card} title="Best speed" value="643" />
+      <TitledCard className={styles.card} title="Average speed" value="531" />
     </div>
   )
 }
