1059184bbff18609d8bf075193ae7de5f5e00676
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-08 04:20:16 +0300

    reworked: now contains 2 files with chars and sizes

---
+++ client/src/constants/characters.ts
@@ -0,0 +1,24 @@
+const RU_CHARACTERS =
+  "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя".split("")
+
+const ENG_CHARACTERS =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("")
+
+const SPECIAL_CHARACTERS = [
+  "Backspace",
+  ..." !@#$%^&*()-_=+~`|/,.<>[]{}№;?".split(""),
+  '"',
+  "'",
+  "\u005C", // this is "\"
+]
+
+const NUMBERS = "0123456789".split("")
+
+const CHARACTERS = [
+  ...RU_CHARACTERS,
+  ...ENG_CHARACTERS,
+  ...SPECIAL_CHARACTERS,
+  ...NUMBERS,
+]
+
+export { CHARACTERS }
