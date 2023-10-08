83c8cd3bda88cc47fd042aab200f8639f6153608
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-08 04:21:07 +0300

    added new test texts

--- client/src/text.ts
+++ client/src/text.ts
@@ -11,4 +11,12 @@ const shortWords =
   "последний отдохнуть ложный возможный прямой труд подождать найти сезон \
 старший обеспечить фраза призванный организованный питерский щека хлеб"
 
-export { longWords, shortWords }
+const testWords = "последний отдохнуть ложный"
+
+const engTestWords =
+  "boohho rubbery silicon whether wassail control whoever instead roengutine"
+
+const engLongWords =
+  "rake psst plot blah only soon blah less from unto mail edge lest idea drat amid pull blah lest flax past dull mini very hmph bind trim till till next trap hake pish lord root fast data hmph spursome ouch even soft what past rust more withdead"
+
+export { longWords, shortWords, testWords, engTestWords, engLongWords }
