d5e9f61e155d54463a7d9df6d9229aa2754b4289
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-08 04:56:12 +0300

    updated styles & logic

--- client/src/components/containers/TextContainer/TextContainer.tsx
+++ client/src/components/containers/TextContainer/TextContainer.tsx
@@ -6,7 +6,14 @@ import type { TextContainerProps } from "./TextContainerProps"
 import styles from "./TextContainer.module.scss"
 
 const TextContainer: FC<TextContainerProps> = ({ children }) => {
-  return <div className={styles.container}>{children}</div>
+  return (
+    <div
+      style={{ width: `${window.innerWidth - 200}px` }}
+      className={styles.container}
+    >
+      {children}
+    </div>
+  )
 }
 
 export default TextContainer
