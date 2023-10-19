2acaed3d0530d61f405ff2e242ebf5b3dc5cc374
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-19 23:40:34 +0300

    addednew Settings sections

---
+++ client/src/components/Settings/AccountSettings.tsx
@@ -0,0 +1,27 @@
+import React, { forwardRef } from "react"
+
+import Text from "components/UI/Text/Text"
+import Dropdown from "components/Dropdown/Dropdown"
+import RangeInput from "components/UI/RangeInput/RangeInput"
+import Checkbox from "components/Checkbox/Checkbox"
+
+import styles from "./styles.module.scss"
+
+const AccountSettings = forwardRef<HTMLSpanElement>(
+  function AccountSettings(props, ref) {
+    return (
+      <>
+        <Text ref={ref} className={styles.title} value="Account" />
+        <Dropdown>
+          <Text value="Item 1" />
+          <Text value="Item 2" />
+          <Text value="Item 3" />
+        </Dropdown>
+        <RangeInput initialValue="50" />
+        <Checkbox textValue="Enable function" initialValue={false} />
+      </>
+    )
+  },
+)
+
+export default AccountSettings
