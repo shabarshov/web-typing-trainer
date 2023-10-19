bb223dfe4b724b372f443fa9b2360ca462aa6ffc
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-19 05:46:12 +0300

    deleted due to refactoring

--- client/src/components/UI/Square/Square.tsx
+++
@@ -1,7 +0,0 @@
-import { SquareProps } from "./SquareProps"
-import { FC } from "react"
-import cl from "./Square.module.scss"
-import cn from "classnames"
-export const Square: FC<SquareProps> = ({ className, children }) => {
-  return <div className={cn(className, cl.sq)}>{children}</div>
-}
