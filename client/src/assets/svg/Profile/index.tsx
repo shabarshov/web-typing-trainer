a91d9dc29828c2f0e071407880a349102fa224db
denissav203@gmail.com <denissav203@gmail.com>
2023-10-21 18:47:48 +0300

    Added new Profile icon

--- client/src/assets/svg/Profile/index.tsx
+++ client/src/assets/svg/Profile/index.tsx
@@ -1,4 +1,7 @@
 import EditIcon from "./EditIcon"
 import ProfileIcon from "./ProfileIcon"
+import BlockedIcon from "./BlockedIcon"
+import UnblockedIcon from "./UnblockedIcon"
+import AchievementIcon from "./AchievementIcon"
 
-export { EditIcon, ProfileIcon }
+export { EditIcon, ProfileIcon, AchievementIcon, BlockedIcon, UnblockedIcon }
