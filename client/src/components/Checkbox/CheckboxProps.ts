a0505e2d0aa2ecd42d2e8d9f98ecc14840f03cea
Alexey Shabarshov <shabarshov3228@mail.ru>
2023-10-24 19:01:21 +0300

    added dispatch prop

--- client/src/components/Checkbox/CheckboxProps.ts
+++ client/src/components/Checkbox/CheckboxProps.ts
@@ -1,6 +1,7 @@
 interface CheckboxProps {
   initialValue: boolean
   textValue: string
+  dispatch?: () => void
 }
 
 export type { CheckboxProps }
--- client/src/components/Dropdown/DropdownProps.ts
+++ client/src/components/Dropdown/DropdownProps.ts
@@ -1,5 +1,7 @@
 interface DropdownProps {
   children: JSX.Element[]
+  initialTitle?: string
+  dispatch?: (value: string) => void
 }
 
 export type { DropdownProps }
