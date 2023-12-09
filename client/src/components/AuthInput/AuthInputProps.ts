5700a1e03d4eb57ad3c359b466bc9fd8407bc0d6
denissav203@gmail.com <denissav203@gmail.com>
2023-12-09 23:37:12 +0300

    Add new props

--- client/src/components/AuthInput/AuthInputProps.ts
+++ client/src/components/AuthInput/AuthInputProps.ts
@@ -1,6 +1,9 @@
 import type { InputProps } from "components/UI/Input/InputProps"
+import { Dispatch, SetStateAction } from "react"
 
 interface AuthInputProps extends InputProps {
+  state: string
+  setState: Dispatch<SetStateAction<string>>
   title?: string
 }
 
